22c9a56893b3f4eed072c5a8af3ef9d8beef4ffc
ser9ga <77145864+ser9ga@users.noreply.github.com>
2021-04-24 13:49:51 +0300

    123

--- index.js
+++ index.js
@@ -39,9 +39,12 @@ function calculateResult(buffer) {
 app.post("/back", (req, res) => {
   res.json(calculateResult(req.body));
 });
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "index.html"));
-});
+
+app.use("/",  express.static(path.join(__dirname, "client")));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "client", "index.html"));
+// });
 
 app.listen(port, () => {
   console.log("Ready to do big dids");
